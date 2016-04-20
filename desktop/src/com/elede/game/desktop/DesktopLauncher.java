0437aa5c9b7ba81a9cb4ad0527fa7009f276a5a4
eLeDe <little_elede@hotmail.com>
2016-04-20 20:07:01 +0200

    segundo commit

---
+++ desktop/src/com/elede/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.elede.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.elede.game.GameTestMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameTestMain(), config);
+	}
+}
