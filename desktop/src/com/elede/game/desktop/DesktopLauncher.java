8234bbc6f84021e9ec9bfb1c1855c2296e264cf6
eLeDe <little_elede@hotmail.com>
2016-04-20 20:16:14 +0200

    Changed Default Window Size

--- desktop/src/com/elede/game/desktop/DesktopLauncher.java
+++ desktop/src/com/elede/game/desktop/DesktopLauncher.java
@@ -7,6 +7,12 @@ import com.elede.game.GameTestMain;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		
+		config.width = 640;
+		config.height = 480;
+		
 		new LwjglApplication(new GameTestMain(), config);
+		
+		
 	}
 }
